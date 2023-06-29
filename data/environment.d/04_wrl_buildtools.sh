0bef6d86c71fd7bd3f7c6a972ac4efd61289b56c
Changqing Li <changqing.li@windriver.com>
2023-06-29 14:28:18 +0800

    04_wrl_buildtools.sh: update minimal gcc version to 9.3.1
    Issue: LIN1023-1141
    
    qt6 need at least gcc9+, so update minimal gcc version to 9.3.1 to
    ensure extended buildtools is used when host gcc lower than gcc 9.3.1
    
    Refer:
    https://doc.qt.io/qt-6/linux-building.html#compilers-development-packages
    
    Signed-off-by: Changqing Li <changqing.li@windriver.com>

--- data/environment.d/04_wrl_buildtools.sh
+++ data/environment.d/04_wrl_buildtools.sh
@@ -43,9 +43,9 @@ buildtools_setup() {
 		BUILDTOOLSBRANCH="${BASEBRANCH}"
 	fi
 
-        #ccache 4.7.2 need c++17 filesystem library, gcc support std::filesystem from 8.1.0
+	#qt6 require at least gcc9+
 	gcc_cur_ver=$(gcc -dumpfullversion -dumpversion 2>/dev/null)
-	required_ver=8.1.0
+	required_ver=9.3.1
 
 	# check whether host gcc version less than $required_ver
 	if [ ! "$(printf '%s\n' "$required_ver" "$gcc_cur_ver" | sort -V | head -n1)" = "$required_ver" ]; then
